bcca020097022fab73edd58c38e53174885f4209
DATABASESK <server1.2148652652@gmail.com>
2025-02-14 21:02:37 +0530

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBZ5hQ08JQt0VYZCv_BRhnw_jC0rTAeeTE",
   authDomain: "skyt-782c3.firebaseapp.com",
   projectId: "skyt-782c3",
-  storageBucket: "skyt-782c3.firebasestorage.app",
+  storageBucket: "skyt-782c3.appspot.com",
   messagingSenderId: "597345210065",
   appId: "1:597345210065:web:03227bfa15114bcf3cbff0"
 };
