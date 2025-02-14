15955cb69158552459b0babddb60d2b805302428
DATABASESK <server1.2148652652@gmail.com>
2025-02-14 21:36:23 +0530

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// Access the environment variables with process.env
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
